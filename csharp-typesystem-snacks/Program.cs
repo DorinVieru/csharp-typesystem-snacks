f2a53668665b7da54d85d6542bb20f6390f0662b
DorinVieru <dorino.vieru@gmail.com>
2024-04-17 15:17:57 +0200

    UP: Complete snack 1

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // SNACK 1
+            Console.WriteLine("Inserisci il primo numero:");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Inserisci il secondo numero:");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            // Verifica del numero maggiore
+            if (numero1 > numero2)
+            {
+                Console.WriteLine($"Il numero maggiore è: {numero1}");
+            }
+            else if (numero2 > numero1)
+            {
+                Console.WriteLine($"Il numero maggiore è: {numero2}");
+            }
+            else
+            {
+                Console.WriteLine("I numeri sono uguali.");
+            }
         }
     }
 }
