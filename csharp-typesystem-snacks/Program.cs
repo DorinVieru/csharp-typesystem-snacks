76b1aeb08cdc16d127480a00ba6bb1b691eebb8f
DorinVieru <dorino.vieru@gmail.com>
2024-04-17 15:51:09 +0200

    UP: Complete Snack 4

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            // SNACK 1
+            /* ---------
+             SNACK 1
+            */
             Console.WriteLine("Inserisci il primo numero:");
             int numero1 = Convert.ToInt32(Console.ReadLine());
 
@@ -25,7 +27,9 @@
                 Console.WriteLine("I numeri sono uguali.");
             }
 
-            // SNACK 2
+            /* ---------
+             SNACK 2
+            */
             Console.WriteLine("Inserisci una parola a tua scelta:");
             string word1 = Convert.ToString(Console.ReadLine());
 
@@ -52,7 +56,9 @@
                 Console.WriteLine("Le parole hanno la stessa lunghezza.");
             }
 
-            // SNACK 3
+            /* ---------
+             SNACK 3
+            */
             int sum = 0; 
 
             // Chiedo all'utente di inserire un numero per 10 volte
@@ -66,6 +72,24 @@
             }
 
             Console.WriteLine("La somma di tutti i numeri inseriti è: " + sum);
+
+            /* ---------
+             SNACK 4
+            */
+            int somma = 0;
+            double media = 0;
+
+            // Ciclo i numeri da 2 a 10 compresi
+            for (int numero = 2; numero <= 10; numero++)
+            {
+                somma += numero; 
+            }
+
+            // Calcolo la media
+            media = (double)somma / 9; 
+
+            Console.WriteLine("La somma dei numeri compresi da 2 a 10 è: " + somma);
+            Console.WriteLine("La media dei numeri compresi da 2 a 10 è: " + media);
         }
     }
 }
